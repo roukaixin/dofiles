0ff6116355b4ff2601e998cd8cc728f607d59c45
pankx <a3427173515@163.com>
2024-04-06 22:13:07 +0800

    refactor: 修改主键为 alt

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -47,7 +47,7 @@ editor_cmd = terminal .. " -e " .. editor
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
+modkey = "Mod1"
 -- }}}
 
 -- {{{ Function definitions(函数(方法)定义)
