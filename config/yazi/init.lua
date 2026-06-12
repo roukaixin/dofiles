93b6c0b419dfb66bad0b6251312b997e9276f75b
roukaixin <a3427173515@163.com>
2026-06-12 15:27:42 +0800

    chore: 添加 yazi 插件

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,4 +1,7 @@
-require("full-border"):setup()
+require("full-border"):setup {
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+}
 
 function Status:name()
     local h = self._tab.current.hovered
